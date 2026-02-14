1071639fb0b79f9766e61a3c3c5e4d97ad9eab8a
biblius <crazycompanyxxl@gmail.com>
2026-02-14 11:35:21 +0100

    add tooltip component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import { ModeWatcher } from 'mode-watcher';
+  import * as Tooltip from '$lib/components/ui/tooltip/index.js';
+
   import './layout.css';
 
   const { children } = $props();
 </script>
 
 <ModeWatcher />
-{@render children()}
+
+<Tooltip.Provider>
+  {@render children()}
+</Tooltip.Provider>
